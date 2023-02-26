58f6d612e789b020145b64b3c20b60ee0c9aea0a
Charles Wang <charlescello123@gmail.com>
2023-02-25 22:17:26 -0500

    imports in main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #Program to generate poetic texts with recurring neural networks. 
 
+import random
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Activation
+from tensorflow.keras.optimizers import RMSprop
 
 
-def main():
-    return 0
